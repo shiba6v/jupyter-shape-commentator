30ad5a710e5a5d4d08ec6fc954d67b230d258eb2
shiba6v <shiba6v@gmail.com>
2019-07-15 00:04:12 +0900

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+## Installation
+```
+jupyter nbextension install https://github.com/shiba6v/jupyter-shape-commentator/archive/master.zip --user
+```
---
+++ jupyter-shape-commentator.js
@@ -0,0 +1,56 @@
+define([
+    'base/js/namespace',
+    ],
+    function (Jupyter) {
+        "use strict";
+        function shape_comment(){
+            var parent = document.getElementsByClassName("end_space")[0];
+            while (parent.firstChild) {
+                parent.removeChild(parent.firstChild);
+            }
+            var cell = Jupyter.notebook.get_selected_cell();
+            var orig_text = cell.get_text();
+            cell.set_text("%%shape_comment"+"\\n"+orig_text);
+            cell.execute();
+        }
+
+        function shape_erase(){
+            var parent = document.getElementsByClassName("end_space")[0];
+            while (parent.firstChild) {
+                parent.removeChild(parent.firstChild);
+            }
+            var cell = Jupyter.notebook.get_selected_cell();
+            var orig_text = cell.get_text();
+            cell.set_text("%%shape_erase"+"\\n"+orig_text);
+            cell.execute();
+        }
+        function load_ipython_extension() {
+            // if (document.getElementById('shape_commentator_button')!=null){
+            //     document.getElementById('shape_commentator_button').remove();
+            // }
+            if (document.getElementById('shape_commentator_button')==null) {
+                Jupyter.toolbar.add_buttons_group([{
+                    'label': 'Shape',
+                    'icon': 'fa-comment',
+                    'callback': shape_comment,
+                    'id': 'shape_commentator_button'
+                }]);
+            }
+
+            // if (document.getElementById('shape_commentator_erase_button')!=null){
+            //     document.getElementById('shape_commentator_erase_button').remove();
+            // }
+            if (document.getElementById('shape_commentator_erase_button')==null) {
+                Jupyter.toolbar.add_buttons_group([{
+                    'label': 'Shape',
+                    'icon': 'fa-eraser',
+                    'callback': shape_erase,
+                    'id': 'shape_commentator_erase_button'
+                }]);
+            }
+        }
+        return {
+            load_ipython_extension: load_ipython_extension
+        };
+    }
+);
